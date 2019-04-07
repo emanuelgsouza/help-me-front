3c4c68e80b80bbbc5c090d4ffa08dd73ed5fbfa8
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-04-07 09:30:16 -0300

    fix isUserLoading getter

--- src/store/auth/getters.js
+++ src/store/auth/getters.js
@@ -4,4 +4,4 @@ export const hasUser = state => !isEmpty(state.user)
 
 export const username = state => get(state, 'user.name', '')
 
-export const isUserLoading = state => state.userLoading || false
+export const isUserLoading = state => state.loadingUser
