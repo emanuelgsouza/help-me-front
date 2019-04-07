851c50839c1c2fe7520028e160aa30a7a17aa8f6
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-04-07 09:19:38 -0300

    update link to repository

--- src/modules/Home/components/GithubCorners.vue
+++ src/modules/Home/components/GithubCorners.vue
@@ -1,5 +1,5 @@
 <template functional>
-  <a href="https://github.com/emanuelgsouza/help-me-front" class="github-corner" aria-label="View source on GitHub">
+  <a href="https://github.com/emanuelgsouza/help-me" class="github-corner" aria-label="View source on GitHub">
     <svg
       width="80"
       height="80"
