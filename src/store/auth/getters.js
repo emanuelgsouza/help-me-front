8dc5d7b31db4501748130bd8fd53bbfa7a6b05b3
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-04-20 17:15:40 -0300

    update username getter in vuex

--- src/store/auth/getters.js
+++ src/store/auth/getters.js
@@ -1,8 +1,17 @@
-import { isEmpty, get } from 'lodash'
+import { isEmpty, get, first, last } from 'lodash'
 
 export const hasUser = state => !isEmpty(state.user)
 
-export const username = state => get(state, 'user.name', '')
+export const username = state => {
+  const name = get(state, 'user.name', '')
+  if (isEmpty(name)) {
+    return name
+  }
+
+  const firstName = first(name.split(' '))
+  const lastName = last(name.split(' '))
+  return `${firstName} ${lastName}`
+}
 
 export const isUserLoading = state => state.loadingUser
 
