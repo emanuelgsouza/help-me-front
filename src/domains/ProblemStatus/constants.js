2603d913cab66750a0e289934b23c2afae3d2750
Emanuel Souza <emanuelgdesouza@gmail.com>
2019-06-24 23:10:58 -0300

    add more status

--- src/domains/ProblemStatus/constants.js
+++ src/domains/ProblemStatus/constants.js
@@ -9,30 +9,46 @@ export const PROBLEM_STATUS = {
     value: 'PENDING_APPROVE'
   },
   PENDING: {
-    name: 'Pendente de apreciação',
-    label: 'Pendente de apreciação',
-    description: 'O problema uma vez aprovado, estará pendente de apreciação por algum aluno ou grupo de alunos da Unigranrio, para que seja executado um projeto',
+    name: 'Pendente de Aprovação',
+    label: 'Pendente de Aprovação',
+    description: 'O problema uma vez aprovado, estará pendente de aprovação por algum aluno ou grupo de alunos da Unigranrio, para que seja executado um projeto',
     value: 'PENDING'
   },
   APPRECIATED: {
-    name: 'Apreciado pela equipe de TI',
-    label: 'Apreciado pela equipe de TI',
+    name: 'Aprovado',
+    label: 'Aprovado',
     description: 'Neste status, dizemos que o problema foi "resolvido"',
     value: 'APPRECIATED'
+  },
+  ARCHIVED: {
+    name: 'Arquivado',
+    label: 'Arquivado',
+    description: 'Um problema que já foi resolvido ou não, mas que não será mais contemplado para algum projeto',
+    value: 'ARCHIVED'
+  },
+  REJECTED: {
+    name: 'Rejeitado',
+    label: 'Rejeitado',
+    description: 'Diz-se do problema que não pode ser contemplado como um projeto de TI',
+    value: 'REJECTED'
   }
 }
 
 export const PROBLEM_STATUS_CONSTANTS = {
   PENDING: 'PENDING',
   PENDING_APPROVE: 'PENDING_APPROVE',
-  APPRECIATED: 'APPRECIATED'
+  APPRECIATED: 'APPRECIATED',
+  ARCHIVED: 'ARCHIVED',
+  REJECTED: 'REJECTED'
 }
 
 /**
  * @type {Object}
  */
 export const STATUS_COLOR = {
-  PENDING_APPROVE: 'negative',
+  PENDING_APPROVE: 'warning',
   PENDING: 'orange',
-  APPRECIATED: 'positive'
+  APPRECIATED: 'positive',
+  ARCHIVED: 'grey',
+  REJECTED: 'negative'
 }
